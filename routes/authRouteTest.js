cdf97dc5d4240ea51a3fa447e2db98bbeb6d892f
Stsiapan Palianski <emslim66@gmail.com>
2024-09-12 17:26:55 +0300

    authRoutePlus

--- routes/authRouteTest.js
+++ routes/authRouteTest.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const UserService = require("../mongo/services/userService");
 
-router.post("/:hash", async (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const telegramData = req.body;
+    const { username, id } = verifyInitData(telegramData.initData);
+    UserService.create({
+      name: username,
+      telegramId: id,
+    });
+    return res.send("success");
+  } catch (error) {
+    return res.send(error);
+  }
+});
 
 module.exports = router;
