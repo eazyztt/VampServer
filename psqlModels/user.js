90caec09851366bd1887d6417a4f8f284efda4e4
Stsiapan Palianski <emslim66@gmail.com>
2024-10-25 16:44:44 +0300

    minor

--- psqlModels/user.js
+++ psqlModels/user.js
@@ -1,5 +1,5 @@
 const { DataTypes, Model } = require("sequelize");
-const sequelize = require("../");
+const sequelize = require("../psqlDb");
 const Task = require("./task");
 const Friendship = require("./friendship");
 
