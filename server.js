355f9e636c69770ffc679e8a071e15b70078013d
Stsiapan Palianski <emslim66@gmail.com>
2025-02-12 21:58:06 +0300

    try2

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(
 const verifyAuth = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const auth2 = req.headers["auth2"];
-  console.log(JSON.stringify(auth2));
+  console.log("Объект:", JSON.stringify(auth2, null, 2), typeof auth2);
 
   console.log(`${authHeader} this is auth header`);
 
