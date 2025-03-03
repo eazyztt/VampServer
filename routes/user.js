3db0866b85d0bbc07588e6575a7bcd2c5f7caa67
Stsiapan Palianski <emslim66@gmail.com>
2025-03-03 21:53:04 +0300

    plus check

--- routes/user.js
+++ routes/user.js
@@ -32,7 +32,7 @@ router.get("/start", async (req, res) => {
       money: 1000,
       readyToClaim: true,
     });
-    if (id && typeof id !== "undefined") {
+    if (id && typeof id !== "undefined" && id != "undefined") {
       const userFriend = await FriendService.addUniqueFriend(id, req.tgId);
       if (!userFriend) {
         return res.send("error");
